ac6fb73281081f6b6aee4ef1d99f9bb0b516d5b9
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-05-23 16:52:00 +0900

    corrected the points that were pointed out

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 20170522073849) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
