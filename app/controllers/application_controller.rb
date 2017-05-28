c8368caf361141e0348c407c683d796487df8ccb
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-05-28 21:41:19 +0900

    html -> haml hash rocket -> simbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resources)
     root_path
   end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
