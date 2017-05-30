862a17e5a925d9bfa7ea7908c7bd809d250e2fac
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-05-30 18:31:47 +0900

    bundle install 'pry-rails'

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,11 @@
 class GroupsController < ApplicationController
 
 def new
+  @group = Group.new
 end
 
 def create
+  Group.create(group_params)
 
 end
 
@@ -15,4 +17,8 @@ def update
 
 end
 
+private
+def group_params
+  params.require(:group).permit()
+end
 end
