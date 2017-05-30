3e25811fcbf9495500d0ea3f6b4974add8051f04
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-05-31 05:31:55 +0900

    collection__check_boxes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,7 @@ end
 
 def create
   Group.create(group_params)
-
+  redirect_to root_path
 end
 
 def edit
@@ -19,6 +19,6 @@ end
 
 private
 def group_params
-  params.require(:group).permit()
+  params.require(:group).permit(:name, {user_ids: []})
 end
 end
