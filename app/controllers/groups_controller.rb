8b7c8a080d418b276fe6e1be8ae38fcbadb39f17
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-06-02 19:27:48 +0900

    corrected the place which were pointed out

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,11 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.create(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "チャットグループが作成されました。"
     else
-      render new_group_path
+      render :new
     end
   end
 
