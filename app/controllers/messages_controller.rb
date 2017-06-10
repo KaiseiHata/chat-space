84ddb7ae5de13fdb754eb996d00bf11069ae0fe2
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-06-10 18:48:23 +0900

    index action and create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,21 @@
 class MessagesController < ApplicationController
-
+  before_action :set_existing_value, only: [:index, :create]
   def index
-    @groups = current_user.groups
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+  end
+
+  private
+  def set_existing_value
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+  end
+
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: @group.id, user_id: current_user.id)
   end
 end
