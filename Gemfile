b14c0ddf0601affd037bfd071b1525a1c0d15aeb
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-06-11 23:50:25 +0900

    introduced carrierwave

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'font-awesome-rails'
 
 gem 'devise'
 
+gem 'carrierwave'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = Message.create(message_params)
+    redirect_to group_messages_path(@message.group_id)
   end
 
   private
@@ -16,6 +17,6 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:body).merge(group_id: @group.id, user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(group_id: @group.id, user_id: current_user.id)
   end
 end
