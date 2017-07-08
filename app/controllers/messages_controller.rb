2a44e42325c1640aa5456fe6e45afb8703866956
KAISEI\Kaisei Hata <hat10w01u@i.softbank.jp>
2017-07-09 06:37:20 +0900

    add json to messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@message.group_id)
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@message.group_id) }
+        format.json
+      end
     else
       flash.now[:alert] = "メッセージが投稿されませんでした"
       render :index
